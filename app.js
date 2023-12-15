a300e3126e3f7f51e1ec6fe2c879b6fe066802f7
Sourav Bera <82562370+souravbera003@users.noreply.github.com>
2023-12-15 20:27:01 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+let grocery = document.getElementById('grocery');
+grocery.addEventListener('submit', addItem);
+function addItem(e){
+    e.preventDefault();
+    let data = this.elements.writeList.value;
+    let list =document.querySelector('ol');
+    let item =document.createElement('li');
+    let text =document.createElement('p');
+
+    text.textContent = data;
+    this.elements.writeList.value="";
+    item.append(text);
+    list.append(item);
+
+    let rmvBtn =document.createElement('spna');
+    rmvBtn.classList.add('remove');
+    item.append(rmvBtn);
+    rmvBtn.addEventListener('click',deleteItem);
+    item.addEventListener('click',doneItem)
+}
+function deleteItem(e){
+    this.parentElement.remove();
+}
+function doneItem(e){
+    this.classList.toggle('done');
+}
